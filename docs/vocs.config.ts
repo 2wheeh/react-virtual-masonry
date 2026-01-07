87284232f31788c275ff5bc01c8c0daddcbea85c
Wonhee Lee <2wheeh@gmail.com>
2026-01-07 16:56:15 +0900

    feat: migrate docs to vocs

---
+++ docs/vocs.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vocs';
+
+export default defineConfig({
+  description: 'Modern Masonry Layout with ease, powered by @tanstack/virtual',
+  title: 'React Virtual Masonry',
+  rootDir: 'contents',
+  twoslash: {
+    compilerOptions: {
+      strict: true,
+    },
+  },
+});
